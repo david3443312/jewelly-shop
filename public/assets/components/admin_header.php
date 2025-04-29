08ac5ecc14ba5b2a95ecdfe2fd190b08501ab16a
david3443312 <pardox2k4@gmail.com>
2025-04-29 07:49:42 +0700

    n

--- public/assets/components/admin_header.php
+++ public/assets/components/admin_header.php
@@ -42,7 +42,7 @@
             </a>
             <a href="#" class="db-decorate">
                 <span class="material-icons-sharp">insights</span>
-                <h4>Analytics</h4>
+                <h4>Collection</h4>
             </a>
             <a href="admin_message.php" class="db-decorate <?php echo ($current_page == 'admin_message.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">mail_outline</span>
@@ -54,11 +54,11 @@
             </a>
             <a href="#" class="db-decorate">
                 <span class="material-icons-sharp">report</span>
-                <h4>Reports</h4>
+                <h4>Material</h4>
             </a>
             <a href="#" class="db-decorate">
                 <span class="material-icons-sharp">settings</span>
-                <h4>Settings</h4>
+                <h4>Special Occasion Jewelry</h4>
             </a>
             <a href="add_products.php" class="db-decorate <?php echo ($current_page == 'add_products.php') ? 'active' : ''; ?>">
                 <span class="material-icons-sharp">add</span>
