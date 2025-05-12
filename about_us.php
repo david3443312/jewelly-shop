96509225e02da42d30b1b10ae4dbaf3aac4d10f2
david3443312 <pardox2k4@gmail.com>
2025-05-12 15:45:27 +0700

    ok

--- about_us.php
+++ about_us.php
@@ -1,23 +1,28 @@
 <?php
-// Trang Giới thiệu về Jewelry PTITshop
+    include '../jewelry-shop/public/assets/components/connect.php';
+
+    $user_id = isset($_COOKIE['user_id']) ? $_COOKIE['user_id'] : null;
+
 ?>
 <!DOCTYPE html>
 <html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Về chúng tôi - Jewelry PTITshop</title>
-    <link rel="stylesheet" href="public/assets/css/user_header.css">
-    <link rel="stylesheet" href="public/assets/css/styleshomepage.css">
-    <link rel="stylesheet" href="public/assets/css/shop.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="icon" href="public/assets/images/logoicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="public/assets/css/about_us.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Trang chủ - Jewelry Shop</title>
+        <link rel="stylesheet" href="public/assets/css/user_header.css">
+        <link rel="stylesheet" href="public/assets/css/styleshomepage.css">
+        <link rel="stylesheet" href="public/assets/css/slider.css">
+        <link rel="stylesheet" href="public/assets/css/shop.css">
+        <link rel="stylesheet" href="public/assets/css/about_us.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="https://fonts.googleapis.com/css2?family=Cormorant+Upright:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+        <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+        <link rel="icon" href="../jewelry-shop/public/assets/images/logoicon.png" type="image/x-icon">
+    </head>
 <body>
 <?php include "public/assets/components/user_header.php"; ?>
 <div class="about-container">
